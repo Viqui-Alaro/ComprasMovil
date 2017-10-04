01c8786a2117e7c7e4f399114d9f87a29ca94ba0
jcarlos79t <jcarlos79t@hotmail.com>
2017-10-04 17:15:51 -0400

    puedes ver viqui??

--- app/src/main/java/com/it/mobile/hansa/hbm/MainActivity.java
+++ app/src/main/java/com/it/mobile/hansa/hbm/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // cambios jeje
+        // cambios jejejokokokoko
 
 
         //Toolbar toolbar = (Toolbar) findViewById(R.id.appbarMain);
